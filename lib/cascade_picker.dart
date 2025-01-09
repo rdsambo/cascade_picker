df3da302cffce75eac4063b626f08602947a4ed3
rdsambo <rdsambo@gmail.com>
2025-01-09 07:09:13 +0200

    using ButtomSheet

--- lib/cascade_picker.dart
+++ lib/cascade_picker.dart
@@ -2,49 +2,65 @@ library cascade_picker;
 
 import 'package:flutter/material.dart';
 
-/// 级联选择器
-/// 使用示例:
-/// ```dart
-/// CascadePicker的page是ListView，没有约束的情况下它的高度是无限的，
-/// 因此需要约束高度。
-///
-/// final _cascadeController = CascadeController();
-///
-/// initialPageData: 第一页的数据
-/// nextPageData: 下一页的数据，点击当前页的选择项后调用该方法加载下一页
-///   - pageCallback: 用于传递下一页的数据给CascadePicker
-///   - currentPage: 当前是第几页
-///   - selectIndex: 当前选中第几项
-/// controller: 控制器，用于获取已选择的数据
-/// maxPageNum: 最大页数
-/// selectedIcon: 已选中选项前面的图标，flutter package不能放本地资源文件，因此需要从外部传入，图标在images文件夹下面
-///
-/// Expand(
-///   child: CascadePicker(
-///     initialPageData: ['a', 'b', 'c', 'd'],
-///     nextPageData: (pageCallback, currentPage, selectIndex) async {
-///       pageCallback(['one', 'two', 'three'])
-///     },
-///     controller: _cascadeController,
-///     maxPageNum: 4,
-///     selectedIcon: Image.asset("images/ic_select_mark.png", width: 10, height: 10, color: Colors.redAccent,),
-/// )
-///
-/// InkBox(
-///   child: Container(...)
-///   onTap: () {
-///     /// 判断是否完成选择
-///     if (_cascadeController.isCompleted()) {
-///       List<String> selectedTitles = _cascadeController.selectedTitles;
-///       List<int> selectedIndexes = _cascadeController.selectedIndexes;
-///     }
-///   }
-/// )
-/// ```
+class CascadePickerWidget extends StatelessWidget {
+  final List<String> initialPageData;
+  final NextPageCallback nextPageData;
+  final int maxPageNum;
+  final CascadeController controller;
+  final Color tabColor;
+  final double tabHeight;
+  final TextStyle tabTitleStyle;
+  final double itemHeight;
+  final TextStyle itemTitleStyle;
+  final Color itemColor;
+  final Widget? selectedIcon;
 
-/// pageData: 下一页的数据
-/// currentPage: 当前是第几页,
-/// selectIndex: 当前页选中第几项
+  const CascadePickerWidget({
+    required this.initialPageData,
+    required this.nextPageData,
+    this.maxPageNum = 3,
+    required this.controller,
+    this.tabHeight = 40,
+    this.tabColor = Colors.white,
+    this.tabTitleStyle = const TextStyle(color: Colors.black, fontSize: 14),
+    this.itemHeight = 40,
+    this.itemColor = Colors.white,
+    this.itemTitleStyle = const TextStyle(color: Colors.black, fontSize: 14),
+    this.selectedIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          builder: (context) => SizedBox(
+            height: MediaQuery.of(context).size.height * 0.8, // Configura a altura do modal
+            child: CascadePicker(
+              initialPageData: initialPageData,
+              nextPageData: nextPageData,
+              maxPageNum: maxPageNum,
+              controller: controller,
+              tabHeight: tabHeight,
+              tabColor: tabColor,
+              tabTitleStyle: tabTitleStyle,
+              itemHeight: itemHeight,
+              itemColor: itemColor,
+              itemTitleStyle: itemTitleStyle,
+              selectedIcon: selectedIcon,
+            ),
+          ),
+        ).whenComplete(() {
+          controller.saveState(); // Salva o estado ao fechar o modal
+        });
+      },
+      child: const Text('Open Cascade Picker'),
+    );
+  }
+}
+
 typedef void NextPageCallback(Function(List<String>) pageData, int currentPage, int selectIndex);
 
 class CascadePicker extends StatefulWidget {
@@ -81,7 +97,7 @@ class CascadePicker extends StatefulWidget {
 
 class _CascadePickerState extends State<CascadePicker> with SingleTickerProviderStateMixin {
 
-  static String _newTabName = "请选择";
+  static String _newTabName = "Select";
 
   final CascadeController _cascadeController;
 
@@ -100,23 +116,15 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
   GlobalKey _sliderKey = GlobalKey();
   List<GlobalKey> _tabKeys = [];
 
-  /// 选择器数据集合
   List<List<String>> _pagesData = [];
-  /// 已选择的title集合
   List<String> _selectedTabs = [_newTabName];
-  /// 已选择的item index集合
   List<int> _selectedIndexes = [-1];
 
-  /// "请选择"tab宽度，添加新的tab时用到
   double _animTabWidth = 0;
-  /// tab添加事件记录，用于隐藏"请选择"tab初始化状态
   bool _isAddTabEvent = false;
-  /// tab移动未开始，渲染'请选择'tab时隐藏文本，这时的tab在终点位置
   bool _isAnimateTextHide = false;
 
-  /// 防止_moveSlider重复调用
   bool _isClickAndMoveTab = false;
-  /// 当前选择的页面，移动滑块前赋值
   int _currentSelectPage = 0;
 
   _addTab(int page, int atIndex, String currentPageItem) {
@@ -127,19 +135,19 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
     widget.nextPageData((data) {
       final nextPageDataIsEmpty = data.isEmpty;
       if (!nextPageDataIsEmpty) {
-        /// 下一页有数据，更新本页数据或添加新的页面
+
         setState(() {
           if (isUpdatePage) {
-            /// 更新下一页
+
             _pagesData[page] = data;
             _selectedTabs[page] = _newTabName;
             _selectedIndexes[page] = -1;
-            /// 清空下下页以后的所有页面和tab数据
+
             _pagesData.removeRange(page + 1, _pagesData.length);
             _selectedIndexes.removeRange(page + 1, _selectedIndexes.length);
             _selectedTabs.removeRange(page + 1, _selectedTabs.length);
           } else {
-            /// 添加新的页面
+
             _isAnimateTextHide = true;
             _isAddTabEvent = true;
             _pagesData.add(data);
@@ -151,17 +159,17 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
           });
         });
       } else {
-        /// 如果下一页数据为空，那么更新本页数据
+
         final currentPage = page - 1;
         setState(() {
           _selectedTabs[currentPage] = currentPageItem;
           _selectedIndexes[currentPage] = atIndex;
-          /// 下一页数据为空，清空下一页以后的所有页面和tab数据
+
           _pagesData.removeRange(page, _pagesData.length);
           _selectedIndexes.removeRange(page, _selectedIndexes.length);
           _selectedTabs.removeRange(page, _selectedTabs.length);
           WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-            // 调整滑块位置
+
             _moveSlider(currentPage);
           });
         });
@@ -171,9 +179,6 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
 
   _moveSlider(int page, {bool movePage = true, bool isAdd = false}) {
     if (movePage && _currentSelectPage != page) {
-      /// 上一次选择的页面和本次选择的页面不同时，移动tab标签，
-      /// 移动时先把_isClickAndMoveTab设为true，防止滑动PageView
-      /// 时_moveSlider重复调用。
       _isClickAndMoveTab = true;
     }
     _isAddTabEvent = isAdd;
@@ -199,12 +204,10 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
     }
   }
 
-  /// 注意：tab渲染完成才开始动画，即调用moveSlider，这个方法会在动画执行期间多次调用
   Widget _animateTab({required Widget tab}) {
     return Transform.translate(
       offset: Offset(Tween<double>(begin: _isAddTabEvent ? -_animTabWidth : 0, end: 0).evaluate(_curvedAnimation), 0),
       child: Opacity(
-        /// 动画未开始前隐藏文本
           opacity: _isAnimateTextHide ? 0 : 1,
           child: tab
       ),
@@ -248,7 +251,6 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
     return widgets;
   }
 
-  /// 选择项
   Widget _pageItemWidget(int index, int page, String item) {
     return GestureDetector(
       child: Container(
@@ -277,31 +279,24 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
       ),
       onTap: () {
         if (page == widget.maxPageNum - 1) {
-          /// 当前页是最后一页
           setState(() {
             _selectedTabs[page] = item;
             _selectedIndexes[page] = index;
-            /// 调整滑块位置
             WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
               _moveSlider(page);
             });
           });
         } else if (_tabKeys.length >= widget.maxPageNum || page < _tabKeys.length - 1) {
           if (index == _selectedIndexes[page]) {
-            /// 选择相同的item
             _moveSlider(page + 1);
           } else {
-            /// 选择不同的item，更新tab renderBox
             setState(() {
               _selectedTabs[page] = item;
               _selectedIndexes[page] = index;
-//              _selectedIndexes.removeRange(page + 1, _selectedIndexes.length);
             });
             _loadNextPageData(page + 1, index, item, isUpdatePage: true);
           }
         } else {
-          /// 添加新tab页面
-          /// page == _tabKeys.length - 1 && _tabKeys.length == widget.maxPageNum
           _selectedTabs[page] = item;
           _selectedIndexes[page] = index;
           _addTab(page + 1, index, item);
@@ -322,7 +317,12 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
   @override
   void initState() {
     super.initState();
-    _pagesData.add(widget.initialPageData);
+
+    widget.controller.restoreState();
+
+    if (widget.controller.isFirstInteraction) {
+      _pagesData.add(widget.initialPageData);
+    }
 
     _controller = AnimationController(
         duration: const Duration(milliseconds: 500),
@@ -399,15 +399,35 @@ class _CascadePickerState extends State<CascadePicker> with SingleTickerProvider
 }
 
 class CascadeController {
-  late final _CascadePickerState _state;
+  _CascadePickerState? _state;
 
-  _setState(_CascadePickerState state) {
+  bool isFirstInteraction = true;
+  List<String>? _savedSelectedTabs;
+  List<int>? _savedSelectedIndexes;
+  List<List<String>>? _savedPagesData;
+
+  void _setState(_CascadePickerState state) {
     _state = state;
   }
 
-  List<String> get selectedTitles => _state._selectedTabs;
+  void saveState() {
+    isFirstInteraction = false;
+    if (_state != null) {
+      _savedSelectedTabs = List<String>.from(_state!._selectedTabs);
+      _savedSelectedIndexes = List<int>.from(_state!._selectedIndexes);
+      _savedPagesData = List<List<String>>.from(_state!._pagesData);
+    }
+  }
 
-  List<int> get selectedIndexes => _state._selectedIndexes;
+  void restoreState() {
+    if (_state != null && _savedSelectedTabs != null && _savedSelectedIndexes != null && _savedPagesData != null) {
+      _state!._selectedTabs = List<String>.from(_savedSelectedTabs!);
+      _state!._selectedIndexes = List<int>.from(_savedSelectedIndexes!);
+      _state!._pagesData = List<List<String>>.from(_savedPagesData!);
+    }
+  }
 
-  bool isCompleted() => !_state._selectedTabs.contains(_CascadePickerState._newTabName);
+  List<String> get selectedTitles => _state!._selectedTabs;
+  List<int> get selectedIndexes => _state!._selectedIndexes;
+  bool isCompleted() => !_state!._selectedTabs.contains(_CascadePickerState._newTabName);
 }
